226dcf69524fcb55b6b6cf6bd4994b2322b7abe8
gloire beniche <gloirebeniche11@gmail.com>
2023-06-05 16:28:47 +0100

    first commit

---
+++ viho-bootstrap-5-admin-dashboard-template/viho/theme/storage/framework/views/6bb4f252f9db7a87bb8aca1aef0b2a47611cf0e6.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/prajit/Downloads/wetransfer_poco-zip_2022-07-16_0710/Viho-Laravel-8/theme/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
